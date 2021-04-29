d2d43ec73402c02a12eee13e8ae398c8d1f2f9dd
robbinb <robbinblomkvist@gmail.com>
2021-04-29 11:55:10 +0200

    small update

--- assets/js/jsgo2.js
+++ assets/js/jsgo2.js
@@ -1,1 +1,1 @@
-$(document).ready(function () { $(window).scroll(function () { if ($(document).scrollTop() > 100) { $("nav").addClass("scrolled"); } else { $("nav").removeClass("scrolled"); } }); });
+$(document).ready(function () { $(window).scroll(function () { if ($(document).scrollTop() > 50) { $("nav").addClass("scrolled"); } else { $("nav").removeClass("scrolled"); } }); });
